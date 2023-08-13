b34e1608c566592b4755a5eeb0ad68b9141c19b1
deronel <seferon90@mail.ru>
2023-08-13 20:27:23 +0300

    jjjjj

---
+++ encryption.py
@@ -0,0 +1,47 @@
+import pyAesCrypt
+import os
+
+
+
+def encryption(file,password):
+
+
+    buffer_size = 524*1024
+
+
+    pyAesCrypt.encryptFile(
+        str(file),
+        str(file) + ".crp",
+        password,
+        buffer_size
+        
+    )
+    print("Файл'" + str(os.path.splitext(file)[0]) + "'зашифрован]")
+    
+    os.remove(file)
+
+def  walking_by_dirs(dir, password):
+
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        if os.path.isfile(path):
+           try:
+              encryption(path, password)
+           except Exception as ex:
+               print(ex) 
+        else:
+            walking_by_dirs(path, password)
+
+password = input("Введите пароль для шифрования:")
+walking_by_dirs("/home/user/Рабочий стол/skraping0", password)
+
+
+
+
+
+        
+
+
+
+
